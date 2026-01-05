c6b162cb3f74739ac162690e0b89bfd21a952e3b
Jaden <100730821+JadenLabs@users.noreply.github.com>
2026-01-05 02:34:07 -0700

    feat(utils): add `is_staff` utility function for staff role and permissions checks

---
+++ src/utils/checks.py
@@ -0,0 +1,14 @@
+from discord import Member
+from discord.ext.commands import bot
+
+from src.base.config import config
+
+def is_staff(member: Member):
+    if member.guild_permissions.administrator:
+        return True
+    elif member.id in config.sudo_users:
+        return True
+    elif config.roles["staff"] in member.roles:
+        return True
+
+    return False
